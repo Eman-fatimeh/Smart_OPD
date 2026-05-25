9a021689d494268a66b8a217dec05ead1b01339b
Eman-fatimeh <fatimaakhan01602@gmail.com>
2026-05-24 23:57:43 -0800

    Please push ho jao

--- backend/main.py
+++ backend/main.py
@@ -79,11 +79,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # or your frontend URL
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
-
 # =========================
 # MODELS
 # =========================
