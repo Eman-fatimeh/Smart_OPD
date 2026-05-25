ea016f7be5836441c0ffbe043942f700ec101031
Eman-fatimeh <fatimaakhan01602@gmail.com>
2026-05-24 23:29:24 -0800

    main.py updated

--- backend/main.py
+++ backend/main.py
@@ -346,3 +346,12 @@ def get_tokens():
         }
         for t in tokens
     ]
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000))
+    )   
